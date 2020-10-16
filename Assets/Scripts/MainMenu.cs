c4d815df77d80503de96a6c6ca8ff769ea03f597
faller <fallermagie@yahoo.fr>
2020-10-16 11:51:58 +0200

    Creation du projet
    -Création de 3 scenes (SampleScene, MainMenu et Credits)
    -Importation des tilemaps
    -Création d'un script de menu
    -Mise en place de la scène MainMenu avec les 3 boutons et l'event system
    -Un début de mapping du niveau
    -Ajout de Texte dans la scène Credits

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayMenu ()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void CreditsMenu()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void QuitMenu ()
+    {
+        Debug.Log("Fermeture du jeu");
+        Application.Quit(); 
+    }
+}
