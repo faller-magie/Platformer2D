1f56ffb998778651f14f83612e53ae8396b81a3f
faller <fallermagie@yahoo.fr>
2020-10-19 16:51:38 +0200

    Installation de packages
    -URP
    -Cinemachine

--- Assets/Scripts/ControllerBehaviour.cs
+++ Assets/Scripts/ControllerBehaviour.cs
@@ -11,6 +11,7 @@ public class ControllerBehaviour : MonoBehaviour
 
     private Controls controls;
     private Vector2 direction;
+    private Rigidbody2D myRB;
 
     private void OnEnable()
     {
@@ -18,6 +19,12 @@ public class ControllerBehaviour : MonoBehaviour
         controls.Enable();
         controls.Main.Move.performed += OnMovePerfomed;
         controls.Main.Move.canceled += OnMoveCanceled;
+        controls.Main.Jump.performed += OnJumpPerformed;
+    }
+
+    private void OnJumpPerformed(InputAction.CallbackContext obj)
+    {
+        myRB.AddForce(Vector2.up * speed, ForceMode2D.Impulse);
     }
 
     private void Move_performed(InputAction.CallbackContext obj)
@@ -34,6 +41,11 @@ public class ControllerBehaviour : MonoBehaviour
     private void OnMoveCanceled(InputAction.CallbackContext obj)
     {
         direction = Vector2.zero;
+    }
+
+    private void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate()
