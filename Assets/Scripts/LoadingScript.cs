05c5f7607a458432a0c90da289656fd994497b38
faller <fallermagie@yahoo.fr>
2020-10-23 12:10:52 +0200

    Transition
    Création de l'écran de chargement :
    -Animations & Animator
    -Script Loading
    -Canvas & prefab LoadingScreen

---
+++ Assets/Scripts/LoadingScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingScript : MonoBehaviour
+{
+
+    [SerializeField] private GameObject loadingScreen; //Initialization du Canvas de chargement
+    [SerializeField] private string SceneToLoad;
+
+
+    public void LoadScene(string SceneToLoad)
+    {
+        StartCoroutine(Load(SceneToLoad));
+    }
+
+    private IEnumerator Load(string SceneToLoad)
+    {
+        var Loading_ScreenInstance = Instantiate(loadingScreen);
+        DontDestroyOnLoad(Loading_ScreenInstance);
+        var loadingAnimator = Loading_ScreenInstance.GetComponent<Animator>();
+        var AnimationTime = loadingAnimator.GetCurrentAnimatorStateInfo(0).length;
+        var loading = SceneManager.LoadSceneAsync(SceneToLoad);
+
+        loading.allowSceneActivation = false;
+
+        while (!loading.isDone)
+        {
+            if (loading.progress >= 0.9f)
+            {
+                loading.allowSceneActivation = true;
+                loadingAnimator.SetTrigger("Disparition");
+            }
+
+            yield return new WaitForSeconds(AnimationTime);
+
+        }
+
+    }
+}
